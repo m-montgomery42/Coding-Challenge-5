c8e490c4326abebf83bfabb70311fb7d770f4387
Markayla Montgomery <mm42@usf.edu>
2025-02-05 18:21:36 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1: Object Properties
+// Declare an object customer with name, age, and email
+const customer = {
+    name: "Adam Baker",
+    age: 27,
+    email: "adam.baker@email.com"
+};
+// Log each property using console.log and a template literal
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer email: ${customer.email}`);
